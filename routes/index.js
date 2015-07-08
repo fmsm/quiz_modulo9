bfb46353d35a00e92f02362c7c90d9fe11c3945b
Francisco Sanchez <daw1@debian>
2015-07-08 12:52:41 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 var quizController = require ('../controllers/quiz_controller');
 
-/* GET home page. */
+// GET pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-//router.get('/quizes/question', quizController.question);
-//router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  //autoload :quizId
 
 // Definición de routas de /quizes
 router.get('/quizes', quizController.index);
@@ -19,6 +19,5 @@ router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 router.get('/author', function(req,res) {
   res.render('author');
 });
-
 
 module.exports = router;
